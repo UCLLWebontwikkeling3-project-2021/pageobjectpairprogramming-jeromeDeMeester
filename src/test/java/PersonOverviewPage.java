ee4ec7caf71967de2ee69d19412e9f3429c96372
Jerome de Meester <jerome.demeester@student.ucll.be>
2020-12-01 14:31:08 +0100

    added test classes

---
+++ src/test/java/PersonOverviewPage.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+
+public class PersonOverviewPage extends Page {
+
+    public PersonOverviewPage(WebDriver driver) {
+        super(driver);
+        this.driver.get(getPath()+"?command=Overview");
+    }
+
+    public boolean containsUserWithEmail(String email) {
+        ArrayList<WebElement> listItems=(ArrayList<WebElement>) this.driver.findElements(By.cssSelector("td"));
+        boolean found=false;
+        for (WebElement listItem:listItems) {
+            if (listItem.getText().contains(email)) {
+                found=true;
+            }
+        }
+        return found;
+    }
+
+    public boolean containsUserWithUserId(String userid) {
+        ArrayList<WebElement> listItems=(ArrayList<WebElement>) this.driver.findElements(By.cssSelector("td"));
+        boolean found=false;
+        for (WebElement listItem:listItems) {
+            if (listItem.getText().contains(userid)) {
+                found=true;
+            }
+        }
+        return found;
+    }
+
+
+}
